fbccc53c4c23343319779148154a707c9e1006eb
Sascha <sascha.derungs@gmail.com>
2017-07-26 10:53:59 +0800

    refactoring

--- index.js
+++ index.js
@@ -10,30 +10,21 @@ app.use(express.static('public'));
 
 const TOKEN = process.env.FB_VERIFY_TOKEN;
 const ACCESS = process.env.FB_ACCESS_TOKEN;
+const APP_SECRET = process.env.FB_APP_SECRET;
 
 app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
-// app.get('/webhook/', function (req, res) {
-//     if (req.query['hub.verify_token'] === TOKEN) {
-//         console.log("Validating webhook");
-//         res.send(req.query['hub.challenge']);
-//     } else {
-//         console.error("Failed validation. Make sure the validation tokens match.");
-//         res.send('No entry');
-//     }
-// });
-
 app.get('/webhook', function (req, res) {
     if (req.query['hub.mode'] === 'subscribe' &&
         req.query['hub.verify_token'] === TOKEN) {
-            console.log("Validating webhook");
-            res.status(200).send(req.query['hub.challenge']);
-  } else {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
         console.error("Failed validation. Make sure the validation tokens match.");
         res.sendStatus(403);
-  }
+    }
 });
 
 app.post('/webhook', function (req, res) {
@@ -42,29 +33,29 @@ app.post('/webhook', function (req, res) {
     // Make sure this is a page subscription
     if (data.object === 'page') {
 
-                // Iterate over each entry - there may be multiple if batched
-                data.entry.forEach(function (entry) {
-                    var pageID = entry.id;
-                    var timeOfEvent = entry.time;
+        // Iterate over each entry - there may be multiple if batched
+        data.entry.forEach(function (entry) {
+            var pageID = entry.id;
+            var timeOfEvent = entry.time;
 
-                    // Iterate over each messaging event
-                    entry.messaging.forEach(function (event) {
-                        if (event.message) {
-                            receivedMessage(event);
-                        } else if (event.postback) {
-                            receivedPostback(event);
-                        } else {
-                            console.log("Webhook received unknown event: ", event);
-                        }
-                    });
-                });
+            // Iterate over each messaging event
+            entry.messaging.forEach(function (event) {
+                if (event.message) {
+                    receivedMessage(event);
+                } else if (event.postback) {
+                    receivedPostback(event);
+                } else {
+                    console.log("Webhook received unknown event: ", event);
+                }
+            });
+        });
 
-            // Assume all went well.
-            //
-            // You must send back a 200, within 20 seconds, to let us know
-            // you've successfully received the callback. Otherwise, the request
-            // will time out and we will keep trying to resend.
-            res.sendStatus(200);
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know
+        // you've successfully received the callback. Otherwise, the request
+        // will time out and we will keep trying to resend.
+        res.sendStatus(200);
     }
 });
 
@@ -96,8 +87,8 @@ function receivedMessage(event) {
                 sendTextMessage(senderID, messageText);
         }
     } else if (messageAttachments) {
-                sendTextMessage(senderID, "Message with attachment received");
-            }
+        sendTextMessage(senderID, "Message with attachment received");
+    }
 }
 
 function receivedPostback(event) {
@@ -119,11 +110,11 @@ function receivedPostback(event) {
 
 function sendTextMessage(recipientId, messageText) {
     var messageData = {
-                recipient: {
-                id: recipientId
+        recipient: {
+            id: recipientId
         },
         message: {
-                text: messageText
+            text: messageText
         }
     };
 
@@ -132,39 +123,39 @@ function sendTextMessage(recipientId, messageText) {
 
 function sendGenericMessage(recipientId) {
     var messageData = {
-                recipient: {
-                id: recipientId
+        recipient: {
+            id: recipientId
         },
         message: {
-                attachment: {
+            attachment: {
                 type: "template",
                 payload: {
-                template_type: "generic",
+                    template_type: "generic",
                     elements: [{
-                title: "rift",
+                        title: "rift",
                         subtitle: "Next-generation virtual reality",
                         item_url: "https://www.oculus.com/en-us/rift/",
                         image_url: "http://messengerdemo.parseapp.com/img/rift.png",
                         buttons: [{
-                type: "web_url",
+                            type: "web_url",
                             url: "https://www.oculus.com/en-us/rift/",
                             title: "Open Web URL"
                         }, {
-                type: "postback",
+                            type: "postback",
                             title: "Call Postback",
                             payload: "Payload for first bubble",
                         }],
                     }, {
-                title: "touch",
+                        title: "touch",
                         subtitle: "Your Hands, Now in VR",
                         item_url: "https://www.oculus.com/en-us/touch/",
                         image_url: "http://messengerdemo.parseapp.com/img/touch.png",
                         buttons: [{
-                type: "web_url",
+                            type: "web_url",
                             url: "https://www.oculus.com/en-us/touch/",
                             title: "Open Web URL"
                         }, {
-                type: "postback",
+                            type: "postback",
                             title: "Call Postback",
                             payload: "Payload for second bubble",
                         }]
@@ -178,27 +169,57 @@ function sendGenericMessage(recipientId) {
 }
 
 function callSendAPI(messageData) {
-                request({
-                    uri: 'https://graph.facebook.com/v2.6/me/messages',
-                    qs: { access_token: ACCESS },
-                    method: 'POST',
-                    json: messageData
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: ACCESS },
+        method: 'POST',
+        json: messageData
 
-                }, function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        var recipientId = body.recipient_id;
-                        var messageId = body.message_id;
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
 
-                        console.log("Successfully sent generic message with id %s to recipient %s",
-                            messageId, recipientId);
-                    } else {
-                        console.error("Unable to send message.");
-                        console.error(response);
-                        console.error(error);
-                    }
-                });
-            }
+            console.log("Successfully sent generic message with id %s to recipient %s",
+                messageId, recipientId);
+        } else {
+            console.error("Unable to send message.");
+            console.error(response);
+            console.error(error);
+        }
+    });
+}
+
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from 
+ * the App Dashboard, we can verify the signature that is sent with each 
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+    var signature = req.headers["x-hub-signature"];
+
+    if (!signature) {
+        // For testing, let's log an error. In production, you should throw an 
+        // error.
+        console.error("Couldn't validate the signature.");
+    } else {
+        var elements = signature.split('=');
+        var method = elements[0];
+        var signatureHash = elements[1];
+
+        var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+            .update(buf)
+            .digest('hex');
+
+        if (signatureHash != expectedHash) {
+            throw new Error("Couldn't validate the request signature.");
+        }
+    }
+}
 
 app.listen(app.get('port'), function () {
-                console.log('Running on port ', app.get('port'));
-            });
+    console.log('Running on port ', app.get('port'));
+});
