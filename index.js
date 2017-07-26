0808735f28cfdfec37c0a348b05619c494539af6
Sascha <sascha.derungs@gmail.com>
2017-07-26 10:46:21 +0800

    refactoring

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const app = express();
+const
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    request = require('request');
+
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const TOKEN = process.env.FB_VERIFY_TOKEN;
 const ACCESS = process.env.FB_ACCESS_TOKEN;
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Hello World');
