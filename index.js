6e47da38fb410b07c0e299585da1ec9f5491a3f5
Sascha <sascha.derungs@gmail.com>
2017-07-26 10:15:59 +0800

    Postback

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ app.post('/webhook', function (req, res) {
             entry.messaging.forEach(function (event) {
                 if (event.message) {
                     receivedMessage(event);
+                } else if (messagingEvent.postback) {
+                    receivedPostback(messagingEvent);
                 } else {
                     console.log("Webhook received unknown event: ", event);
                 }
@@ -87,8 +89,21 @@ function receivedMessage(event) {
     }
 }
 
-function sendGenericMessage(recipientId, messageText) {
-    // To be expanded in later sections
+function receivedPostback(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfPostback = event.timestamp;
+
+    // The 'payload' param is a developer-defined field which is set in a postback 
+    // button for Structured Messages. 
+    var payload = event.postback.payload;
+
+    console.log("Received postback for user %d and page %d with payload '%s' " +
+        "at %d", senderID, recipientID, payload, timeOfPostback);
+
+    // When a postback is called, we'll send a message back to the sender to 
+    // let them know it was successful
+    sendTextMessage(senderID, "Postback called");
 }
 
 function sendTextMessage(recipientId, messageText) {
